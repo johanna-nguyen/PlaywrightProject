1291bead96e769f88778e86c1adb23a7341b288e
Johanna Nguyen <johannanguyenfr@gmail.com>
2026-09-19 07:44:37 +0200

    Delete comment

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,20 +1,6 @@
 import {test, expect} from '@playwright/test';
 
 // TC01: Form Authentication : Login successful with valid credentials
-
-// Open browser
-
-// Navigate to https://the-internet.herokuapp.com/login
-
-// Fill in username with tomsmith
-
-// Fill in the password with SuperSecretPassword!
-
-// Click on Login button
-
-// And the home page is appear
-
-// TC01: Form Authentication : Login successful with valid credentials
 test('login successful', async ({ page }) => {
   await page.goto('https://the-internet.herokuapp.com/login');
   await page.getByRole('textbox', { name: 'Username' }).fill('tomsmith');
